dff97bea90ef9228b3d13e9836820e70a5a667be
Mehmet <81226436+mdizman@users.noreply.github.com>
2021-06-08 16:23:25 +0300

    loginprocess

---
+++ login_islem/login_islem/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace login_islem
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelLoginAnaYazı = new System.Windows.Forms.Label();
+            this.lblKullaniciAdi = new System.Windows.Forms.Label();
+            this.lblSifre = new System.Windows.Forms.Label();
+            this.txtKullaniciAd = new System.Windows.Forms.TextBox();
+            this.txtSifre = new System.Windows.Forms.TextBox();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // labelLoginAnaYazı
+            // 
+            this.labelLoginAnaYazı.AutoSize = true;
+            this.labelLoginAnaYazı.Font = new System.Drawing.Font("Segoe Print", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.labelLoginAnaYazı.ForeColor = System.Drawing.Color.DarkCyan;
+            this.labelLoginAnaYazı.Location = new System.Drawing.Point(88, 39);
+            this.labelLoginAnaYazı.Name = "labelLoginAnaYazı";
+            this.labelLoginAnaYazı.Size = new System.Drawing.Size(154, 37);
+            this.labelLoginAnaYazı.TabIndex = 0;
+            this.labelLoginAnaYazı.Text = "Login Sayfası";
+            // 
+            // lblKullaniciAdi
+            // 
+            this.lblKullaniciAdi.AutoSize = true;
+            this.lblKullaniciAdi.Font = new System.Drawing.Font("Segoe Print", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblKullaniciAdi.ForeColor = System.Drawing.SystemColors.InfoText;
+            this.lblKullaniciAdi.Location = new System.Drawing.Point(25, 169);
+            this.lblKullaniciAdi.Name = "lblKullaniciAdi";
+            this.lblKullaniciAdi.Size = new System.Drawing.Size(127, 28);
+            this.lblKullaniciAdi.TabIndex = 1;
+            this.lblKullaniciAdi.Text = "Kullanıcı Adı :";
+            // 
+            // lblSifre
+            // 
+            this.lblSifre.AutoSize = true;
+            this.lblSifre.Font = new System.Drawing.Font("Segoe Print", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblSifre.ForeColor = System.Drawing.SystemColors.InfoText;
+            this.lblSifre.Location = new System.Drawing.Point(25, 233);
+            this.lblSifre.Name = "lblSifre";
+            this.lblSifre.Size = new System.Drawing.Size(60, 28);
+            this.lblSifre.TabIndex = 2;
+            this.lblSifre.Text = "Şifre :";
+            // 
+            // txtKullaniciAd
+            // 
+            this.txtKullaniciAd.Location = new System.Drawing.Point(158, 175);
+            this.txtKullaniciAd.Name = "txtKullaniciAd";
+            this.txtKullaniciAd.Size = new System.Drawing.Size(180, 20);
+            this.txtKullaniciAd.TabIndex = 3;
+            // 
+            // txtSifre
+            // 
+            this.txtSifre.Location = new System.Drawing.Point(158, 239);
+            this.txtSifre.Name = "txtSifre";
+            this.txtSifre.PasswordChar = '*';
+            this.txtSifre.Size = new System.Drawing.Size(180, 20);
+            this.txtSifre.TabIndex = 4;
+            this.txtSifre.UseSystemPasswordChar = true;
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.BackColor = System.Drawing.Color.DarkCyan;
+            this.btnLogin.ForeColor = System.Drawing.SystemColors.InfoText;
+            this.btnLogin.Location = new System.Drawing.Point(183, 318);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(121, 30);
+            this.btnLogin.TabIndex = 5;
+            this.btnLogin.Text = "Login";
+            this.btnLogin.UseVisualStyleBackColor = false;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.MistyRose;
+            this.ClientSize = new System.Drawing.Size(361, 471);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.txtSifre);
+            this.Controls.Add(this.txtKullaniciAd);
+            this.Controls.Add(this.lblSifre);
+            this.Controls.Add(this.lblKullaniciAdi);
+            this.Controls.Add(this.labelLoginAnaYazı);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Login";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelLoginAnaYazı;
+        private System.Windows.Forms.Label lblKullaniciAdi;
+        private System.Windows.Forms.Label lblSifre;
+        private System.Windows.Forms.TextBox txtKullaniciAd;
+        private System.Windows.Forms.TextBox txtSifre;
+        private System.Windows.Forms.Button btnLogin;
+    }
+}
+
---
+++ login_islem/login_islem/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.Sql;
+using System.Data.SqlClient;
+namespace login_islem
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection con;
+        SqlDataReader dr;
+        SqlCommand com;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            string user = txtKullaniciAd.Text;
+            string password = txtSifre.Text;
+            con = new SqlConnection(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Ornek;Integrated Security=True");
+            com = new SqlCommand();
+            con.Open();
+            com.Connection = con;
+            com.CommandText = "Select*From Kullanici_Bilgi where kullanici_adi='" + txtKullaniciAd.Text + 
+                "'And sifre='" + txtSifre.Text + "'";
+            dr = com.ExecuteReader();
+            if (dr.Read())
+            {
+                MessageBox.Show("Giriş Başarılı");
+                Form2 gecis = new Form2();
+                gecis.Show();
+                this.Hide();
+            }
+            else
+            {
+                MessageBox.Show("Kullanıcı adı veya Şifre hatalı");
+            }
+            con.Close();
+        }
+    }
+}
